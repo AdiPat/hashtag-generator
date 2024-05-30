e497298513c9a120d7280b6c543fc446c2c955cc
AdiPat <contact.adityapatange@gmail.com>
2024-05-31 02:33:13 +0530

    hashtag_utils: get similar hashtags

--- hashtag_utils/hashtag_utils.py
+++ hashtag_utils/hashtag_utils.py
@@ -34,3 +34,29 @@ class HashtagUtils:
         except Exception as e:
             traceback.print_exc()
             return None
+        
+    def get_similar_hashtags(self, tags: List[str], temperature=0.5, num_tags=5) -> List[str]:
+        try:
+            completion = self.client.chat.completions.create(
+                model="gpt-4o",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": f"You are an AI agent that analyses hashtags and generates similar hashtags.\
+                                            The hashtags will be supplied to your in the form of a prompt.\
+                                            Return the result as a JSON-array of the similar hashtags. \
+                                            Generate {num_tags} hashtags for the following hashtags.",
+                    },
+                    {"role": "user", "content": f"Hashtags: {",".join(tags)}"},
+                ],
+                temperature=temperature,
+                response_format={"type": "json_object"},
+            )
+
+            hashtags_json = completion.choices[0].message.content
+            hashtags = json.loads(hashtags_json)
+
+            return hashtags
+        except Exception as e:
+            traceback.print_exc()
+            return None
